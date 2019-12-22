d628c6cc05361123b38a762593a7063f6f677b2d
Jason Rivera <jrivera2850@gmail.com>
2019-12-22 12:25:43 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import Home from './pages/Home';
+import Post from './pages/Post';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import BlogProvider from "./context/BlogProvider";
+import Navbar from "./components/Navbar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BlogProvider>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/:postId' exact component={Post} />
+          </Switch>
+        </Router>
+      </div>
+    </BlogProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import BlogProvider from './context/BlogProvider';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
